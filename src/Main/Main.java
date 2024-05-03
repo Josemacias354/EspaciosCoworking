aa3855fb6ef0d873eec6f28e6431f1f6a09a7bce
Josemacias354 <maciasbichillo1@gmail.com>
2024-05-03 13:36:22 +0200

    update

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,18 +1,27 @@
 package Main;
+import Plataforma.Escritorio;
 import Plataforma.Servicio;
 import Plataforma.Usuario;
 import org.junit.Test;
 
+import java.util.Scanner;
+
 import static org.junit.Assert.assertEquals;
 
 public class Main {
-    public void main() {
-    // Aquí puedes agregar más métodos de prueba para los otros métodos en la clase Usuario
+    public static void main() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Que necesitas?");
+
 
 // Create a new instance of Servicio
 Servicio servicio1 = new Servicio("Impresora", 3.5);
 Servicio servicio2 = new Servicio("Cafeteria", 10.0);
 
+// Create instances of Escritorio
+        Escritorio escritorio1 = new Escritorio(1);
+        Escritorio escritorio2 = new Escritorio(2);
+        Escritorio escritorio3 = new Escritorio(3);
 
     }
 }
